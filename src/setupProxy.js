54c0d2995da2819f252aa33ab3bf5b1d92b3739c
Daniel Macias <josedanielmr@gmail.com>
2021-08-21 02:56:04 -0600

    adasd

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,6 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = app => {
-    app.use(proxy("/api/*", { target: "https://mernbackendtec.herokuapp.com/" }));
+    app.use(proxy("/api/*", { target: "https://mernbackendtec.herokuapp.com" }));
   };
 
